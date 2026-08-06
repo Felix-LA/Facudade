e738c7ada20e0e7d340e37f80e645226289810f1
Felix-LA <felix.lobo85@gmmail.com>
2026-08-05 21:04:19 -0400

    Att 02 - Vetores

---
+++ UC -- 21/Att 02/main.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+/*
+int main(){
+    for (int i = 2; i <= 100; i++){
+        if(i % 2 == 0){
+            printf("%d \n",i);
+        }
+    }
+    return 0;
+}
+*/
+
+/*
+int main(){
+    int num1, num2;
+    printf("Digite o Numero: ");
+    scanf("%d", &num1);
+    printf("Digite o Numero: ");
+    scanf("%d", &num2);
+
+    if(num2 > num1){
+        for (int i = num1; i <= num2; i++){
+            if(i % 2 == 0){
+                printf("%d \n",i);
+            }
+        }
+    }
+    else if (num1 > num2){
+        for (int i = num2; i <= num1; i++){
+            if(i % 2 == 0){
+                printf("%d \n",i);
+            }
+        }
+    }
+    else{
+        printf("Numeros Invalidos\n");
+    }
+    
+    
+    return 0;
+}
+*/
+
+/*
+int main(){
+    int num;
+    printf("Informe o Numero: ");
+    scanf("%d", &num);
+
+    for (int i = 0; i <= 10; i++){
+        int resultado = num * i;
+        printf("%d x %d = %d \n", num,i,resultado);
+    }
+
+    return 0;
+}
+*/
+
+int main(){
+    
+}
